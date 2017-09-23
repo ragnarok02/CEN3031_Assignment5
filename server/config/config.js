42bbba3f10a1a48657f25757fd79d237e2323eed
Cesar Martinez <martc86@gmail.com>
2017-09-22 21:43:52 -0400

    a

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+		  db: {
+		    uri: 'mongodb://test:kh227@ds135624.mlab.com:35624/mydb', //place the URI of your mongo database here.
+		  }, 
+		  googleMaps: {
+		    key: ''
+		  },
+		  port: 8080
+		};
